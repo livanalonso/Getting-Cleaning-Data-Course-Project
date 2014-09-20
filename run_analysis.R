970413a9b4fc3160145dc889f846781cf771e6a3
Livan Alonso <livan.alonso@gmail.com>
2014-09-20 14:38:22 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 # Set working directory
 setwd("~/Documents/Livan/Programming/Courses/Coursera_Getting&CleaningData/Project")
 
-# Download the data for the project
+# Download the data of project
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="dataset.zip",method="curl")
         
@@ -10,15 +10,13 @@ unzip("dataset.zip")
 
 # Remove zip file
 file.remove("dataset.zip")
-dir<-"UCI HAR Dataset"
 
-# path to common files
+# path to common files (train and test data sets)
+dir<-"UCI HAR Dataset"
 path.features<-paste(dir,"/features.txt",sep="")
 path.activity<-paste(dir,"/activity_labels.txt",sep="")
 
-
 # path to train folder files
-# 
 path.measurement_train<-paste(dir,"/train/X_train.txt",sep="")
 path.label_train<-paste(dir,"/train/y_train.txt",sep="")
 path.subject_train<-paste(dir,"/train/subject_train.txt",sep="")
@@ -27,7 +25,6 @@ path.subject_train<-paste(dir,"/train/subject_train.txt",sep="")
 path.measurement_test<-paste(dir,"/test/X_test.txt",sep="")
 path.label_test<-paste(dir,"/test/y_test.txt",sep="")
 path.subject_test<-paste(dir,"/test/subject_test.txt",sep="")
-
 
 # Reading files
 features.data<-read.table(path.features,header = FALSE)
@@ -44,18 +41,20 @@ subject_test.data<-read.table(path.subject_test,header = FALSE)
 
 ################################################################################
 # 1-Merges the training and the test sets to create one data set.
+################################################################################
 
 # Merging train and test data by using rbind() function
 measurement_data<-rbind(measurement_train.data,measurement_test.data)
 label_data<-rbind(label_train.data,label_test.data)
 subject_data<-rbind(subject_train.data,subject_test.data)
 
-# Merging all columns containing subject, activity label and measurements information
+# Merging all columns containing subject, activity labels and measurements
 merged.sets<-cbind(subject_data,label_data,measurement_data)
 
 
 ################################################################################
 # 2-Extracts only the measurements on the mean and standard deviation for each measurement. 
+################################################################################
 
 # By using grepl() function, a logical vector can be created to search matches with mean() and st() labeled columns.
 # By using which() function, indexes can be extrated
@@ -69,6 +68,7 @@ dataset_mean.st<-merged.sets[, c(1:2,2+mean.columns,2+st.columns) ]
 
 ################################################################################
 # 3-Uses descriptive activity names to name the activities in the data set
+################################################################################
 
 # cut() function divides the range of activity.data[,1] into 
 merged.sets[,2]<-cut(merged.sets[,2],breaks=length(activity.data[,1]),as.vector(activity.data[,2]))
@@ -76,6 +76,8 @@ merged.sets[,2]<-cut(merged.sets[,2],breaks=length(activity.data[,1]),as.vector(
 
 ################################################################################
 # 4-Appropriately labels the data set with descriptive variable names. 
+################################################################################
+
 colnames(merged.sets)<-c("subject","activity",as.vector(features.data[,2]))
 
 
@@ -83,3 +85,9 @@ colnames(merged.sets)<-c("subject","activity",as.vector(features.data[,2]))
 ################################################################################
 # 5-From the data set in step 4, creates a second, independent tidy data set with 
 #   the average of each variable for each activity and each subject.
+################################################################################
+all.column.names<-colnames(merged.sets)
+
+dataset.average<-aggregate(merged.sets[,3:length(all.column.names)],
+                           list("subject"=merged.sets$subject,"activity"=merged.sets$activity), mean)
+write.table(dataset.average,file="dataset.step5.txt",row.name=FALSE)
